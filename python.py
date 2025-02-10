550ef2ffa8306b3ee9c4f8bec5cefe4b449174d3
marc aron alfonso <marc.aron.alfonso21@gmail.com>
2025-02-10 16:42:12 +0800

    Secret Number Practice

--- python.py
+++ python.py
@@ -66,4 +66,3 @@ if __name__ == "__main__":
     pay(total)
 
 
-Alfonso
---
+++ secretnumber.py
@@ -0,0 +1,11 @@
+secret_number = 5
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    user_input = int(input("Guess the number between 1 to 5:"))
+    guess_count += 1
+    if user_input == secret_number:
+        print ("Correct!")
+        break
+else:
+  print ("Game over")
